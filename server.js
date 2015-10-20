fda359dc41d7fae663b1cc4c1070b99b076c5f5c
Sasha Aickin <xander76@yahoo.com>
2015-10-20 11:13:42 -0700

    Fixed a problem that made the endpoints with correct client behavior mess up the raw perf stats

--- server.js
+++ server.js
@@ -10,7 +10,30 @@ var RecursiveDivs = require("./RecursiveDivs");
 
 app.use('/static', express.static('static'));
 
+// =========== raw responses ==============
+// these responses (/string and /stream) just read out from ReactDOM and ReactDOMStream to make it easy to measure 
+// TTFB and TTLB. If you want to have a fully functional, connected React page, use /stringClient or /streamClient.
+
 app.get('/string', function (req, res) {
+  var {depth = 1, breadth = 1} = req.query;
+
+  res.write(ReactDOM.renderToString(<RecursiveDivs depth={depth} breadth={breadth}/>));
+  res.end();
+});
+
+app.get('/stream', async function (req, res) {
+  var {depth = 1, breadth = 1} = req.query;
+
+  var hash = await ReactDOMStream.renderToString(<RecursiveDivs depth={depth} breadth={breadth}/>, res, {bufferSize: 10000});
+  res.end();
+});
+
+// =========== client-rendered responses ==========
+// these endpoints show how to connect back to the markup with React on the client side.
+// However, since they read out the <html> open tag immediately, they are not reliable for measuring
+// TTFB.
+
+app.get('/client/string', function (req, res) {
   var {depth = 1, breadth = 1} = req.query;
 
   res.write(`<html><body><div id="renderNode">`);
@@ -26,7 +49,7 @@ app.get('/string', function (req, res) {
   res.end();
 });
 
-app.get('/stream', async function (req, res) {
+app.get('/client/stream', async function (req, res) {
   var {depth = 1, breadth = 1} = req.query;
 
   res.write(`<html><body><div id="renderNode">`);
