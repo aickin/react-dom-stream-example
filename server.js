999c77d2d9e7de849bc7f9e8e1c42b552f1967cf
Sasha Aickin <xander76@yahoo.com>
2015-10-20 09:56:31 -0700

    Moved buffering logic into the core project.

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var ReactDOMStream = require("react-dom-stream/server");
 
 var RecursiveDivs = require("./RecursiveDivs");
 
+app.use('/static', express.static('static'));
+
 app.get('/string', function (req, res) {
   var {depth = 1, breadth = 1} = req.query;
 
@@ -24,40 +26,20 @@ app.get('/string', function (req, res) {
   res.end();
 });
 
-app.use('/static', express.static('static'));
-
 app.get('/stream', async function (req, res) {
   var {depth = 1, breadth = 1} = req.query;
 
-  // for now, we need to buffer some of the stream coming out; express performs really poorly if you 
-  // chunk out a few bytes at a time. I plan to move this functionality into react-dom-stream.
-  var wrappedRes = {
-  	write: function(data) {
-	  this.buffer = this.buffer || "";
-	  this.buffer += data;
-
-	  if (this.buffer.length >= 10000) {
-	  	res.write(this.buffer);
-	  	this.buffer = "";
-	  }
-	},
-
-	end: function(data) {
-		res.write(this.buffer);
-		res.end(data);
-	}
-  }
-  wrappedRes.write(`<html><body><div id="renderNode">`);
-  var hash = await ReactDOMStream.renderToString(<RecursiveDivs depth={depth} breadth={breadth}/>, wrappedRes);
-  wrappedRes.write(`</div>`);
-  wrappedRes.write(`
+  res.write(`<html><body><div id="renderNode">`);
+  var hash = await ReactDOMStream.renderToString(<RecursiveDivs depth={depth} breadth={breadth}/>, res, {bufferSize: 10000});
+  res.write(`</div>`);
+  res.write(`
   	<script src="/static/client-stream.bundle.js"></script>
   	<script type="text/javascript">
   		render(${depth}, ${breadth}, ${hash});
   	</script>
   	`);
-  wrappedRes.write(`</body></html>`);
-  wrappedRes.end();
+  res.write(`</body></html>`);
+  res.end();
 });
 
 var server = app.listen(process.env.PORT, function () {
